0be8710ffab0cd2a0885dbc343496fee142c5bb0
WangWNico <nicowang178@gmail.com>
2024-05-10 12:32:59 -0400

    Mobile Sizing for Projects

--- components/project.tsx
+++ components/project.tsx
@@ -34,7 +34,7 @@ export default function Project({
         className="bg-[#c4eaf8] sm:max-w-[42rem] max-w-[25rem] border border-black-/9 overflow-hidden sm:pr-8 relative sm:h-[20rem] shadow-2xl 
             rounded-[2.5rem] even:pl-8 hover:bg-[#ADD8E6] transition flex flex-col"
       >
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] max-w-[30%] flex flex-col h-full group-even:ml-[18rem]">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] max-w-[30%] flex flex-col h-full sm:group-even:ml-[18rem] group-even:ml-[16rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
@@ -55,12 +55,12 @@ export default function Project({
           className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadown-2xl
             transition
             group-hover:scale-[1.04]
-            group-hover:-translate-x-3 
-            group-hover:translate-y-3 
+            group-hover:-translate-x-3
+            group-hover:translate-y-3
             group-hover:-rotate-2
 
-            group-even:group-hover:translate-x-3 
-            group-even:group-hover:translate-y-3 
+            group-even:group-hover:translate-x-3
+            group-even:group-hover:translate-y-3
             group-even:group-hover:rotate-2
 
             group-even:-right-[initial] 
