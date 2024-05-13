e39c3711c9b01e0833ec8383083b78faa16bbfd7
WangWNico <nicowang178@gmail.com>
2024-05-13 00:59:06 -0400

    IM DONEEE
    Adasdas

--- components/contact.tsx
+++ components/contact.tsx
@@ -57,7 +57,7 @@ function Contact() {
                 <button type="submit" 
                     className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 hover:bg-gray-950
                     text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-10'>
-                        Submit <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1'/>
+                        Submit <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1' ></FaPaperPlane>
                     </button>
             </form>
         </motion.section>
--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,15 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
+import { HiComputerDesktop } from "react-icons/hi2";
 import gamemenuImg from "@/public/game_menu.png";
 import cephPoster from "@/public/Ceph_Poster.jpg";
 import sysflowPoster from "@/public/SysFlow_Poster.jpg";
+import CPU from "@/public/CPU.jpg";
+import QuestListImg from "@/public/QuestList.jpg";
+import MipsImg from "@/public/mips.png";
+
+
 
 export const links = [
   {
@@ -35,28 +41,36 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Cyber Security Analyst | IBM SysFlow",
+    location: "Troy, TX",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "I documented project steps, troubleshooting, paving the way for group members to set up and participate in the project. My team and I ended up analyzing threats and vulnerabilities by creating graphics and Jupiter Notebooks.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2024",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Software Developer | IBM CEPH",
+    location: "Troy, NY",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+      "I worked with a team and mentor from IBM on developing the 'Ceph balancer status' command to provide detailed information on the changes made during the optimization of a Ceph cluster.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2023",
+  },
+  {
+    title: "Started CS Degree and Rensselaer Polytechnic Institute",
+    location: "Troy, NY",
+    description:
+      "Started my college journey.",
+    icon: React.createElement(HiComputerDesktop),
+    date: "2022-2026",
+  },
+  {
+    title: "Graduated Brooklyn Technical High School",
+    location: "Brooklyn, NY",
+    description:
+      "I graduated after 4 years with a specialized diploma in Media studies.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2018-2022",
   },
 ] as const;
 
@@ -76,11 +90,32 @@ export const projectsData = [
     imageUrl: sysflowPoster,
   },
   {
+    title: "QuestList Freelancing Startup",
+    description:
+      "Worked with team to improve the website and user interface.",
+    tags: ["Web Design", "Graphic Design", "Web Development"],
+    imageUrl: QuestListImg,
+  },
+  {
+    title: "CPU Architecture Design",
+    description:
+      "Created a simple CPU architecture.",
+    tags: ["Assembly Language", "Verilog", "MIPS Assembly", 'CPU design'],
+    imageUrl: CPU,
+  },
+  {
     title: "Ceph",
     description:
       "Enhanced the 'Ceph balancer status' command to provide detailed information on the changes made during the optimization of a Ceph cluster.",
     tags: ["Python", "Git", "Data Analytics"],
     imageUrl: cephPoster,
+  },
+  {
+    title: "MIPS Compiler in C",
+    description:
+      "Created a basic compiler for MIPS Assembly in C code.",
+    tags: ["Python", "Git", "Data Analytics"],
+    imageUrl: MipsImg,
   },
 ] as const;
 
